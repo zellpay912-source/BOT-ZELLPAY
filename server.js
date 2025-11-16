1755ab78b3bd6b8c7532b290869b9588b331faaf
zellpay912-source <zellpay912@gmail.com>
2025-11-15 20:06:21 -0500

    Create server.js

---
+++ server.js
@@ -0,0 +1,22 @@
+{
+  "name": "discord-web-sender",
+  "version": "1.0.0",
+  "description": "Discord Bot API for web terminal.",
+  "main": "server.js",
+  "scripts": {
+    "start": "node server.js"
+  },
+  "keywords": [
+    "discord",
+    "bot",
+    "api"
+  ],
+  "author": "",
+  "license": "ISC",
+  "dependencies": {
+    "body-parser": "^1.20.2",
+    "discord.js": "^14.15.3",
+    "dotenv": "^16.4.5",
+    "express": "^4.19.2"
+  }
+}
